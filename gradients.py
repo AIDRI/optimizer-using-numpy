26eef0d0cc41720fba2dda9e00980be456183f8e
AIDRI <ad.israel38@gmail.com>
2021-02-09 20:46:12 +0100

    re-organization

--- gradients.py
+++ gradients.py
@@ -1,5 +1,5 @@
-from sgd_optimizer import SGD_no_momentum, SGD_momentum, SGD_nesterov
-from adam_optimizer import Adam
+from optimizers.sgd_optimizer import SGD_no_momentum, SGD_momentum, SGD_nesterov
+from optimizers.adam_optimizer import Adam
 
 func = lambda w : (1.5 - w[0] + w[0]*w[1])**2 + (2.25 - w[0] + w[0]*w[1]**2)**2 + (2.625 - w[0] + w[0]*w[1]**3)**2
 
